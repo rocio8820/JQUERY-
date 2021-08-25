625d63f09f08db14f3b961a688e61f91d4b06168
rocio <rocio8820@gmail.com>
2021-08-24 19:21:59 -0600

    first commit

---
+++ app.js
@@ -0,0 +1,12 @@
+$('.navItem').each(function(index){
+
+$ (this).css("color","white");
+});
+
+$ ('#nav').css ("background-color","#145");
+
+$('ul li:even').css ("background-color","#ccc");
+
+$('ul li:odd').css("background-color","#aaa");
+
+$("input").val("Rocio");
